698f2ee51a70eb6981af936c1dbab6f832a081cd
ira <vikhrova.irina@gmail.com>
2023-03-30 15:17:21 +0800

    swiper configuring

--- src/index.js
+++ src/index.js
@@ -2,3 +2,62 @@ import './index.html';
 import './css/index.css';
 import './js/header-menu-toggler.js';
 import './js/footer-menu-toggle.js';
+
+const swiperFood = new Swiper('.swiper-food', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 5,
+    spaceBetween: 25,
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+
+        144: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 25,
+        },
+    },
+
+});
+
+
+const mySwiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
