b7ff11da0372f9fff9f149ac51bb2ba10734e041
ira <vikhrova.irina@gmail.com>
2023-04-11 20:17:12 +0600

    finished filter logic JS

--- src/js/food-filter.js
+++ src/js/food-filter.js
@@ -1,13 +1,13 @@
     const selectors = {
         filter: '.popular-filter__item',
+        list: '.swiper-food',
     }
-
     const states = {
         filter: {
             current: 'popular-filter__item--current',
-        }
+        },
+        hidden: 'hidden',
     }
-
 
     const InitFilter = () => {
 
@@ -20,21 +20,27 @@
         })
 
     };
-
     const filter = ({currentTarget}) => {
 
         const filterItems = document.querySelectorAll(selectors.filter);
+        const foodLists = document.querySelectorAll(selectors.list);
+        const foodList = document.querySelector('.swiper-food--' + currentTarget.id)
+
 
         filterItems.forEach((item) => {
             item.classList.remove(states.filter.current);
         })
 
+        foodLists.forEach((list) => {
+            list.classList.add(states.hidden);
+        })
+
         currentTarget.classList.add(states.filter.current);
+        foodList.classList.remove(states.hidden);
+
+
+
 
     };
-
-
-
-
 
     window.addEventListener('DOMContentLoaded', () => { InitFilter() });
